30e02655f6b29e6dc0ee643dc9c38d7150e0c09c
Alex Norman <Aarleks@gmail.com>
2021-06-29 22:25:23 +1000

    Add Teaching Statement

--- website/teaching.html.pm
+++ website/teaching.html.pm
@@ -1,3 +1,18 @@
 #lang pollen
 
-i teach good
+◊topic{Teaching Statement}
+
+Above all else, I approach teaching by trusting students and seeking to be transparent about every aspect of the journey of learning and teaching we will take together.
+
+I value connectedness with learners and creating a sense of belonging for the learning space while aspiring to mastery as a mindset and process. I want my students to be inspired to learn - that is to teach themselves about their subject matter, of course, but also to teach themselves about them as people. I want to help students understand the value of learning not simply as a way of increasing qualification, but as a mechanism of self-understanding and growth. As a teacher, I want my students to leave a class, a course, or a degree eager to learn more and equipped to do so.
+
+My intentions are to help students develop their learning mindset and their abilities to self-learn. In doing this, I want my students to feel they belong together, with me and my teaching colleagues, in the class (whatever 'in the class' might mean). I want my teaching to be a context in which I help set the conditions in which my students can learn.
+
+As an educator, I am trying to help my students develop knowledge, skill, and habits with which to teach themselves and others about the universe.
+
+◊line
+
+I can hear one of my old undergrad teachers still.
+◊blockquote{"Theory and method, theory and method, theory and method"}
+I want my students to understand for themselves that learning 'what' and learning 'how' are two different but necessary vectors for understanding.
+
