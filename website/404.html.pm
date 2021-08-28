d09587cdd94b456458121f7647a0eea0c8b05dbc
Alex Norman <Aarleks@gmail.com>
2021-08-28 15:03:35 +1000

    Added 404 page

---
+++ website/404.html.pm
@@ -0,0 +1,15 @@
+#lang pollen
+
+◊line
+
+◊h1{404!!}
+
+It looks like the page you're looking for has not yet had the privilege of being created.
+
+Should you wish, you can select one of the links to the left to take you to pages more likely to have been created.
+
+◊line
+
+©2016-2021 Alex Norman
+
+Latest update on ◊(date-string)
