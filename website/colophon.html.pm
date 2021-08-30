0eed20f0855de9a1121f649e4aff01e17451b5d2
Alex Norman <Aarleks@gmail.com>
2021-08-31 07:52:10 +1000

    Slight change to colophon

--- website/colophon.html.pm
+++ website/colophon.html.pm
@@ -4,10 +4,11 @@
 
 This site is written in ◊link["https://docs.racket-lang.org/pollen/index.html"]{Pollen}, a programming language for publishing web-books. Pollen is an implementation of Racket developed by the typographer ◊link["https://practicaltypography.com/"]{Matthew Butterick} as a way of being able to call a full programming language from within a document.
 
-Previously, this site was made using ◊link[""]{Markdown} files converted to HTML with ◊link["https://pandoc.org/"]{Pandoc,} which is an excellent document conversion tool. But in ◊in-line{Pollen} I found something more in line with my thinking about documents as programs, that was cleaner than ◊in-line{html} to write in, and had better extensibility than ◊in-line{Markdown.} Plus I was looking for an excuse to tinker on something.
+Previously, this site was made using ◊link[""]{Markdown} files converted to HTML with ◊link["https://pandoc.org/"]{Pandoc}, which is an excellent document conversion tool. But in ◊in-line{Pollen} I found something more in line with my thinking about documents as programs, that was cleaner than ◊in-line{html} to write in, and had better extensibility than ◊in-line{Markdown.} Plus I was looking for an excuse to tinker on something.
 
 ◊ul{
 ◊li{◊strong{Font:} The body text and most titles are ◊link["https://www.ibm.com/plex/"]{IBM Plex} Mono with IBM Plex Sans Condensed for some page titles.}
 ◊li{◊strong{CSS:} I originally riffed from the standard pandoc html and borrowed liberally from ◊link["https://tilde.club/~ejw/"]{Edwin Wenink's} tilde.club website. But over time I've also drawn from the Tufte format, Matthew Butterick's typographic advice in ◊link["https://practicaltypography.com/"]{Practical Typography}, and a few other sites that I've now forgotten.}
-◊li{◊strong{Code:} Code for this site can be found on my ◊link["https://github.com/Aarleks/pollen-tilde"]{Github.}}
+◊li{◊strong{Syntax highlighing:} Code chunks are coloured using ◊link["https://pygments.org/"]{Pygments} in combination with the ◊in-line{◊link["https://docs.racket-lang.org/pollen/Pygments.html"]{Pygments}} function in the ◊in-line{Unstable} Pollen module. I'm using the 'Tango' colourscheme.}
+◊li{◊strong{Code:} The Pollen markup code for this site can be found on my ◊link["https://github.com/Aarleks/pollen-tilde"]{Github.}}
 }
